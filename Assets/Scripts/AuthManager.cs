415328bf7c5fed9d8ba47766290985d8eb5f0e6d
alejo241 <raw-07@hotmail.com>
2020-12-09 16:54:58 +0100

    login
    login

---
+++ Assets/Scripts/AuthManager.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+using Firebase.Auth;
+using TMPro;
+
+
+
+public class AuthManager : MonoBehaviour
+{
+
+    //Firebase variables
+    [Header("Firebase")]
+    public DependencyStatus dependencyStatus;
+    public FirebaseAuth auth;
+
+    //Login variables
+    [Header("Login")]
+    public TMP_InputField inputFieldCorreo;
+    public TMP_InputField inputFieldContraseña;
+   
+    //Register variables
+    [Header("Register")]
+    public TMP_InputField inputFieldRegistroCorreo;
+    public TMP_InputField inputFieldRegistroContraseña;
+    public TMP_InputField inputFieldRegistroContraseñaConfirm;
+
+
+    //Otras variables
+    [Header("Otras variables")]
+    public ControlJuego controlJuego;
+
+    void Awake()
+    {
+        //comprobar que existen todas las dependencias de firebase
+        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task =>
+        {
+            dependencyStatus = task.Result;
+            if (dependencyStatus == DependencyStatus.Available)
+            {
+                //si existen
+                auth = FirebaseAuth.DefaultInstance;
+            }
+            else
+            {
+                Debug.LogError("No estan disponible las dependencias: " + dependencyStatus);
+            }
+        });
+    }
+
+    public void LoginButton()
+    {
+        //Call the login coroutine passing the email and password
+        StartCoroutine(Login());
+    }
+
+    private IEnumerator Login()
+    {
+        var LoginTask = auth.SignInWithEmailAndPasswordAsync(inputFieldCorreo.text, inputFieldContraseña.text);
+        
+        //esperar a que la tarea esté completa
+        yield return new WaitUntil(() => LoginTask.IsCompleted);
+
+
+        //si no hay errores
+        if(LoginTask.Exception == null)
+        {
+            controlJuego.volverMenu();
+        }
+
+    }
+
+    public void registroButton()
+    {
+        //Call the login coroutine passing the email and password
+        StartCoroutine(Registro());
+    }
+
+    private IEnumerator Registro()
+    {
+        var registroTask = auth.CreateUserWithEmailAndPasswordAsync(inputFieldRegistroCorreo.text, inputFieldRegistroContraseña.text);
+
+        //esperar a que la tarea esté completa
+        yield return new WaitUntil(() => registroTask.IsCompleted);
+
+        //si las contraseñas son iguales
+        if (inputFieldRegistroContraseña.text.Equals(inputFieldRegistroContraseñaConfirm.text))
+        {
+            //si no hay errores
+            if (registroTask.Exception == null)
+            {
+                controlJuego.volverMenu();
+            }
+            else
+            {
+                Debug.Log("task error");
+            }
+
+        }
+        else
+        {
+            Debug.Log("pass error");
+        }
+    }    
+      
+
+    
+
+
+
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+
+    public GameObject loginPantalla;
+    public GameObject registroPantalla;
+
+    public void mostrarPantallaLogin()
+    {
+        registroPantalla.SetActive(false);
+        loginPantalla.SetActive(true);
+    }
+
+    public void mostrarPantallaRegistro()
+    {
+        loginPantalla.SetActive(false);
+        registroPantalla.SetActive(true);       
+    }
+
+}
