e176d9f6fcc382be2cacd0fd9a38be14bb9783c0
alejo241 <raw-07@hotmail.com>
2020-12-14 20:49:54 +0100

    cambios en la estetica del nivel y el login

--- Assets/Scripts/actualizarPuntuacion.cs
+++ Assets/Scripts/actualizarPuntuacion.cs
@@ -1,18 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class actualizarPuntuacion : MonoBehaviour
 {
-    Text txtPuntuacion;
+    public TMP_Text txtPuntuacion;
     public static int puntuacion;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        txtPuntuacion = GetComponent<Text> ();
+        //txtPuntuacion = GetComponent<Text> ();
         puntuacion = 0;
         
     }
